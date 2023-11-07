3fe84cf18f29f2ca60d4f5c7c79fe198b5da87a1
fish-puce <xfsarzosa@puce.edu.ec>
2023-11-07 14:49:36 -0500

    agrego js

---
+++ fish.js
@@ -0,0 +1,16 @@
+<!-- FILEPATH: /C:/Users/Fish/Desktop/git/index.html -->
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Formulario de texto</title>
+  </head>
+  <body>
+    <form>
+      <label for="texto">Ingresa un texto:</label>
+      <input type="text" id="texto" name="texto">
+      <button type="submit">Enviar</button>
+    </form>
+    <div id="mensaje"></div>
+  </body>
+</html>
